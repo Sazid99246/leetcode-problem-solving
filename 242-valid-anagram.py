8b10d2f21d72f22080ae7f2a142e78cd921005bf
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2025-07-10 10:39:49 +0600

    valid anagram problem solved

---
+++ 242-valid-anagram.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        else:
+            sorted_s = "".join(sorted(list(s)))
+            sorted_t = "".join(sorted(list(t)))
+            return sorted_s == sorted_t
+
+s = Solution()
+print(s.isAnagram("anagram", "nagaram"))
+print(s.isAnagram("rat", "car"))
