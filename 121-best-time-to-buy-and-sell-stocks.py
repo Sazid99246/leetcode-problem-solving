13493aa495be9c8b4a378c9b4395151c367bfda5
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2025-07-15 12:57:58 +0600

    best time to buy and sell stocks problem solved

---
+++ 121-best-time-to-buy-and-sell-stocks.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if len(prices) < 2:
+            return 0
+
+        min_price = prices[0]
+        max_profit = 0
+        for price in prices:
+            if price < min_price:
+                min_price = price
+            profit = price - min_price
+            if profit > max_profit:
+                max_profit = profit
+
+        return max_profit
+
+
+s = Solution()
+print(s.maxProfit([7, 1, 5, 3, 6, 4]))
+print(s.maxProfit([7, 6, 4, 3, 1]))
